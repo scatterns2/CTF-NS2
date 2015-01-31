bfc9a1a6f2b80b9408bffea318c930db85c05a49
SoulRider <andy@internetaddress.com>
2015-01-31 01:29:59 +0000

    Flag Class change fix
    If an alien changes class while carrying flag, it returns to marine flag
    spawn.

--- lua/Flags/CTF_FlagBearerMixin.lua
+++ lua/Flags/CTF_FlagBearerMixin.lua
@@ -96,10 +96,11 @@ if Server then
     end
     
     function FlagBearerMixin:DetachAll()
-        if team ~= 0 then
+        local team = self:GetTeamNumber()
+        if team >= 1 then
             if (self.attachedFlag ~= nil) then
                 local flag = self.attachedFlag
-                local origin, success = self:GetAttachPointOrigin(GetTeamAttachPoint())
+                local origin, success = self:GetAttachPointOrigin(self:GetFlagAttachPointName())
                 if origin then
                     flag:SetOrigin(origin)
                 end   
